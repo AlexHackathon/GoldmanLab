d7c3ffd5a1cbbc6c28e333c5d49b03ad91f40d05
AlexHackathon <alexandru.tapus@gmail.com>
2025-01-30 09:39:37 -0800

    Working With Refactoring

--- Code/Refactoring/RefactoringMain.py
+++ Code/Refactoring/RefactoringMain.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import FacilitationSim as FacSim
 import Refactoring.SimSupport as SimSupport
 import Helpers.Bound as Bound
@@ -15,7 +17,6 @@ f = 1
 t_f = 2000
 t_s = 50
 
-parameters = FacSim.FacilitationParameters(dt, totalTime,t_s, maxFreq, eyeMin, eyeMax, eyeRes, P0, f, t_f)
 
 timeToKill = 100
 fractionDead = 1
@@ -24,6 +25,7 @@ firstHalf = True
 dataLoc = "../EmreThresholdSlope_NatNeuroCells_All (1).xls"
 
 #Define Facilitation Simulation
+parameters = FacSim.FacilitationParameters(dt, totalTime,t_s, maxFreq, eyeMin, eyeMax, eyeRes, P0, f, t_f)
 sim = FacSim.Simulation(parameters, dataLoc)
 
 w_min = -.005
@@ -31,6 +33,7 @@ w_max = 100
 bounds = [Bound.BoundQuadrants(n, w_min, w_max, sim.neuronNum) for n in range(sim.neuronNum)]
 
 sim.w_mat, sim.T = SimSupport.FitWeightMatrixExclude(sim.r_mat, sim.r_mat_neg, sim.f, bounds)
+
 print("MADE IT!")
 sim.FitPredictorNonlinearSaturation()
 
@@ -39,4 +42,29 @@ for e in range(len(sim.eyePos)):
         eyePos, rVect, tauVect = sim.RunSimF(timeToKill, startIdx=e, dead=SimSupport.GetDeadNeurons(1,True,sim.neuronNum))
         plt.plot(sim.t_vect, eyePos)
         plt.ylim(([eyeMin,eyeMax]))
+plt.show()
+
+tauAvg = []
+for tauF in np.linspace(0,2000,10):
+    # Define Facilitation Simulation
+    parameters = FacSim.FacilitationParameters(dt, totalTime, t_s, maxFreq, eyeMin, eyeMax, eyeRes, P0, f, tauF)
+    sim = FacSim.Simulation(parameters, dataLoc)
+
+    w_min = -.005
+    w_max = 100
+    bounds = [Bound.BoundQuadrants(n, w_min, w_max, sim.neuronNum) for n in range(sim.neuronNum)]
+
+    sim.w_mat, sim.T = SimSupport.FitWeightMatrixExclude(sim.r_mat, sim.r_mat_neg, sim.f, bounds)
+
+    print("MADE IT!")
+    sim.FitPredictorNonlinearSaturation()
+
+    tauVect = []
+    for e in range(len(sim.eyePos)):
+        if e % 1000 == 0:
+            eyePos, rVect, tau = sim.RunSimF(timeToKill, startIdx=e,
+                                                 dead=SimSupport.GetDeadNeurons(1, True, sim.neuronNum))
+            tauVect.append(tau)
+    tauAvg.append(np.average(tauVect))
+plt.plot(np.linspace(0,2000,10), tauAvg)
 plt.show()
--- Code/Refactoring/SimSupport.py
+++ Code/Refactoring/SimSupport.py
@@ -1,21 +1,20 @@
 import scipy
 import numpy as np
-import multiprocessing
+import multiprocessing as mp
 def GetDeadNeurons(fraction, firstHalf, neuronNum):
     if firstHalf:
         return range(0, int(neuronNum // 2 * fraction))
     else:
         return [neuronNum // 2 + j for j in range(0, int(neuronNum // 2 * fraction))]
-def FitHelperParallel(startN, endN, X, bounds, r_mat_neg):
-    for myN in range(startN,endN):
-        r = r_mat_neg[myN]
-        solution = None
-        if bounds != None:
-            bounds = bounds[myN]
-            solution = scipy.optimize.lsq_linear(X, r, bounds)
-        else:
-            solution = scipy.optimize.lsq_linear(X, r)
-        return solution.x[:-1], solution.x[-1]
+def FitHelperParallel(n, X, bounds, r_mat_neg):
+    print(n)
+    r = r_mat_neg[n]
+    solution = None
+    if bounds != None:
+        solution = scipy.optimize.lsq_linear(X, r, bounds[n])
+    else:
+        solution = scipy.optimize.lsq_linear(X, r)
+    return solution
 def FitWeightMatrixExcludeParallel(sim, bounds):
     '''Fit fixed points in the network using target curves.
 
@@ -27,18 +26,23 @@ def FitWeightMatrixExcludeParallel(sim, bounds):
     #Update the program to say that those have been taken
     #When the other core finishes, it updates which it has taken
     #If the last has been taken, continue to writing
+    print("Started fitting")
     X = np.ones((len(sim.eyePos), len(sim.r_mat) + 1))
     for i in range(len(X)):
         for j in range(len(X[0]) - 1):
             X[i, j] = sim.f(sim.r_mat[j, i])
-    p1 = multiprocessing.Process(target=FitHelperParallel, args=(0, sim.neuronNum//2, X, bounds, sim.r_mat_neg, sim))
+    print("Finished X")
+    """p1 = multiprocessing.Process(target=FitHelperParallel, args=(0, sim.neuronNum//2, X, bounds, sim.r_mat_neg, sim))
     p2 = multiprocessing.Process(target=FitHelperParallel, args=(sim.neuronNum//2, sim.neuronNum, X, bounds, sim.r_mat_neg, sim))
     p1.start()
     p2.start()
     p1.join()
     p2.join()
-    sim.FitPredictorNonlinearSaturation()
-
+    sim.FitPredictorNonlinearSaturation()"""
+    print("Started")
+    pool = mp.Pool(5) #
+    result = pool.starmap(FitHelperParallel, [(n,X,bounds, sim.r_mat_neg) for n in range(sim.neuronNum)])
+    return  result
 def FitWeightMatrixExclude(tuningCurves, tuningCurvesNeg, activationFunc, bounds):
     '''Fit fixed points in the network using target curves.
 
